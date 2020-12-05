95bf11d33f8292ef97a85aec45fc5e870b850b03
kokon322 <chornuy322@gmail.com>
2020-12-05 12:34:42 +0100

    add new package, main and enam CarType

---
+++ src/homeWork4/Main.java
@@ -0,0 +1,9 @@
+package homeWork4;
+
+public class Main {
+    public static void main(String[] args) {
+        for (CarType car : CarType.values()) {
+            car.print();
+        }
+    }
+}
