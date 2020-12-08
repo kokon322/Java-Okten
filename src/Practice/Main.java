f900d2f50b0470a7eeaed9a016eabfb4dcaeedc6
kokon322 <chornuy322@gmail.com>
2020-12-08 17:21:36 +0100

    add method getDescription in Seasons

--- src/Practice/Main.java
+++ src/Practice/Main.java
@@ -2,6 +2,7 @@ package Practice;
 
 public class Main {
     public static void main(String[] args) {
-
+       Seasons s = Seasons.SUMMER;
+        System.out.println(s);
     }
 }
--- src/Practice/Seasons.java
+++ src/Practice/Seasons.java
@@ -1,10 +1,37 @@
 package Practice;
 
 public enum Seasons {
-    SPRING,
-    SUMMER,
-    AUTUMN,
-    WINTER
+    SPRING(10){
+        @Override
+        void getDescription() {
+            System.out.println("");
+        }
+    },
+    SUMMER(25){
+        @Override
+        void getDescription() {
+            System.out.println("");
+        }
+    },
+    AUTUMN(12){
+        @Override
+        void getDescription() {
+            System.out.println("");
+        }
+    },
+    WINTER(-22){
+        @Override
+        void getDescription() {
+            System.out.println("");
+        }
+    };
 
+    private int averageTemperature;
 
+    Seasons(int averageTemperature) {
+        this.averageTemperature = averageTemperature;
+    }
+
+    abstract void getDescription();
 }
+
