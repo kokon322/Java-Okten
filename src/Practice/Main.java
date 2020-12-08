85ea4d5453def2c251a7d24b6aef28e4cb25d965
kokon322 <chornuy322@gmail.com>
2020-12-08 17:30:36 +0100

    Finish Practice

--- src/Practice/Main.java
+++ src/Practice/Main.java
@@ -2,7 +2,19 @@ package Practice;
 
 public class Main {
     public static void main(String[] args) {
-       Seasons s = Seasons.SUMMER;
-        System.out.println(s);
+//First task
+//       Seasons s = Seasons.SUMMER;
+//        System.out.println(s);
+//Second task
+//        for (Seasons value : Seasons.values()) {
+//            System.out.println(value);
+//            value.getDescription();
+//            System.out.println(value.getAverageTemperature());
+//        }
+//Last task)
+        Seasons best = Seasons.SUMMER;
+        System.out.println("Любимое время года "+best);
+        best.getDescription();
+
     }
 }
--- src/Practice/Seasons.java
+++ src/Practice/Seasons.java
@@ -1,28 +1,33 @@
 package Practice;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+
 public enum Seasons {
     SPRING(10){
         @Override
         void getDescription() {
-            System.out.println("");
+            System.out.println("Не понятное время года");
         }
     },
     SUMMER(25){
         @Override
         void getDescription() {
-            System.out.println("");
+            System.out.println("Жаркое время года");
         }
     },
     AUTUMN(12){
         @Override
         void getDescription() {
-            System.out.println("");
+            System.out.println("Холодное время года");
         }
     },
     WINTER(-22){
         @Override
         void getDescription() {
-            System.out.println("");
+            System.out.println("Дико холодное время года");
         }
     };
 
