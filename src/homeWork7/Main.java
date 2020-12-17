bceba919c106fd3a215a12b11f38b1b02086c97e
kokon322 <chornuy322@gmail.com>
2020-12-17 10:51:08 +0100

    Add person and book

--- src/homeWork7/Main.java
+++ src/homeWork7/Main.java
@@ -1,10 +1,36 @@
 package homeWork7;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        Book one = new Book("Маленький принц", 2020, 12);
+        Book two = new Book("Коллекционер", 2012, 384);
+        Book three = new Book("Портрет Дориана Грея", 2010, 352);
+        Book four = new Book("Дон Кихот", 1998, 544);
+        Book five = new Book("451' по Фаренгейту", 2010, 320);
+        Book six = new Book("Ярмарка тщеславия", 1995, 928);
+        Book seven = new Book("Идиот", 2002, 640);
 
+        List<Book> first = new ArrayList<>(Arrays.asList(one, five, three));
+        List<Book> second = new ArrayList<>(Arrays.asList(two, six, four));
+        List<Book> third = new ArrayList<>(Arrays.asList(seven, six, one));
+
+        Person igor = new Person(1, "Igor", 28, first);
+        Person maryna = new Person(2, "Maryna", 26, second);
+        Person artem = new Person(3, "Artem", 2, third);
+        Person larysa = new Person(4, "Larysa", 55, second);
+        Person victoria = new Person(5, "Victoria", 38, third);
+        Person marya = new Person(6, "Marya", 16, second);
+        Person anastasia = new Person(7, "Anastasia", 6, first);
+
+        List<Person> allPerson = new ArrayList<>(Arrays.asList(igor, marya, maryna, anastasia, artem, larysa, victoria));
+
+        allPerson.forEach(System.out::println);
 
 
     }
--- src/homeWork7/Person.java
+++ src/homeWork7/Person.java
@@ -1,16 +1,19 @@
 package homeWork7;
 
+import java.util.List;
 import java.util.Objects;
 
 public class Person {
     private int id;
     private String name;
     private int age;
+    private List<Book> bookList;
 
-    public Person(int id, String name, int age) {
+    public Person(int id, String name, int age, List<Book> bookList) {
         this.id = id;
         this.name = name;
         this.age = age;
+        this.bookList = bookList;
     }
 
     public Person() {
@@ -40,17 +43,25 @@ public class Person {
         this.age = age;
     }
 
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Person person = (Person) o;
-        return id == person.id && age == person.age && Objects.equals(name, person.name);
+        return id == person.id && age == person.age && Objects.equals(name, person.name) && Objects.equals(bookList, person.bookList);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, age);
+        return Objects.hash(id, name, age, bookList);
     }
 
     @Override
@@ -59,6 +70,7 @@ public class Person {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", age=" + age +
+                ", bookList=" + bookList +
                 '}';
     }
 }
