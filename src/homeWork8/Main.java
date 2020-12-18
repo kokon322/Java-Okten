a598ca9899d38802a25b11f551769053c42fbaeb
kokon322 <chornuy322@gmail.com>
2020-12-18 22:00:58 +0100

    finish with Exception

--- src/homeWork8/Main.java
+++ src/homeWork8/Main.java
@@ -1,7 +1,43 @@
 package homeWork8;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+//        int x = 2 + 2;
+//        int[] arr = new int[5];
+//        Object a = null;
 
+
+//        try {
+//            a.toString();
+////            System.out.println(arr[6]);
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            System.out.println("______________________________________");
+//            System.out.println("Catch: ArrayIndexOutOfBoundsException");
+//            System.out.println("______________________________________");
+//        } catch (NullPointerException e) {
+//            System.out.println("______________________________________");
+//            System.out.println("Catch: NullPointerException");
+//            System.out.println("______________________________________");
+//        } finally {
+//            System.out.println("Finally");
+//        }
+
+//        try {
+//            myMethod();
+//        } catch (MyException e) {
+//            System.out.println(e.getVeryDetailMessage());
+//        }
+//
+//
+//    }
+//    public static void myMethod() throws MyException{
+//        List<String> list = new ArrayList<>();
+//        if (list.isEmpty()){
+//            throw new MyException("Empty list", "I DONT LIKE EMPTY LIST");
+//        }
+//    }
     }
 }
