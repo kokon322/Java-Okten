f1eb663da07236bd3d4598898fc337f7756f55c3
kokon322 <chornuy322@gmail.com>
2020-12-23 20:59:49 +0100

    Start practice and add class main

--- src/Practice/Main.java
+++ src/Practice/Main.java
@@ -15,23 +15,23 @@ public class Main {
 //        StringBuilder stringBuilder = new StringBuilder("Igor is nice developer");
 //        stringBuilder.replace(8,8,"Very ");
 //        System.out.println(stringBuilder);
-
-        StringBuffer stringBuffer = new StringBuffer("Hello");
-        stringBuffer.append(" ");
-        stringBuffer.append("World");
-        System.out.println(stringBuffer);
-        stringBuffer.append(12);
-        stringBuffer.append(new ArrayList<>(Arrays.asList(12,25,5)));
-        System.out.println(stringBuffer);
-        stringBuffer.delete(11, stringBuffer.length());
-        System.out.println(stringBuffer);
-        stringBuffer.deleteCharAt(0);
-        System.out.println(stringBuffer);
-        stringBuffer.insert(0, "H");
-        System.out.println(stringBuffer);
-        stringBuffer.reverse();
-        System.out.println(stringBuffer);
-        System.out.println(stringBuffer.substring(1,5));
+//
+//        StringBuffer stringBuffer = new StringBuffer("Hello");
+//        stringBuffer.append(" ");
+//        stringBuffer.append("World");
+//        System.out.println(stringBuffer);
+//        stringBuffer.append(12);
+//        stringBuffer.append(new ArrayList<>(Arrays.asList(12,25,5)));
+//        System.out.println(stringBuffer);
+//        stringBuffer.delete(11, stringBuffer.length());
+//        System.out.println(stringBuffer);
+//        stringBuffer.deleteCharAt(0);
+//        System.out.println(stringBuffer);
+//        stringBuffer.insert(0, "H");
+//        System.out.println(stringBuffer);
+//        stringBuffer.reverse();
+//        System.out.println(stringBuffer);
+//        System.out.println(stringBuffer.substring(1,5));
 
     }
 }
