99132f42e73cbd08a2ba9d122d083020e0b422c8
kokon322 <chornuy322@gmail.com>
2020-12-23 22:08:57 +0100

    join

--- src/Practice1/Main.java
+++ src/Practice1/Main.java
@@ -1,7 +1,22 @@
 package Practice1;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+//        MyThread myThread = new MyThread("Full-thread-1", "Igor");
+//        myThread.start();
+
+        Runnable runnable = () -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(Thread.currentThread().getName() + ": "+ i);
+            }
+        };
+        Thread thread = new Thread(runnable, "My-Tread-1");
+        thread.start();
+        thread.join();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + ": "+ i);
+        }
 
     }
 }
