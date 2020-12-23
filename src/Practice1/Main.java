65cc4992ddb791cf782006de74c02f4622a69d4c
kokon322 <chornuy322@gmail.com>
2020-12-23 23:05:28 +0100

    volatile

--- src/Practice1/Main.java
+++ src/Practice1/Main.java
@@ -1,22 +1,65 @@
 package Practice1;
 
 public class Main {
+
+    public static volatile boolean flag = true;
+
     public static void main(String[] args) throws InterruptedException {
 //        MyThread myThread = new MyThread("Full-thread-1", "Igor");
 //        myThread.start();
 
-        Runnable runnable = () -> {
-            for (int i = 0; i < 5; i++) {
-                System.out.println(Thread.currentThread().getName() + ": "+ i);
-            }
-        };
-        Thread thread = new Thread(runnable, "My-Tread-1");
-        thread.start();
-        thread.join();
+//        Runnable runnable = () -> {
+//            try {
+//                for (int i = 0; i < 5; i++) {
+//                    Thread.sleep(1000);
+//                    System.out.println(Thread.currentThread().getName() + ": " + i);
+//                }
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        };
+//
+//        Runnable runnable2 = () -> {
+//            for (int i = 0; i < 10; i++) {
+//                try {
+//                    Thread.sleep(500);
+//                    System.out.println(Thread.currentThread().getName() + ": " + i);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        };
+//
+//        Thread thread1 = new Thread(runnable, "My-Tread-1");
+//        Thread thread2 = new Thread(runnable2, "My-Tread-2");
+//
+//        thread1.start();
+//        thread1.join();
+//
+//        thread2.start();
+//        thread2.join();
+//
+//        for (int i = 0; i < 4; i++) {
+//            System.out.println(Thread.currentThread().getName() + ": " + i);
+//        }
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println(Thread.currentThread().getName() + ": "+ i);
-        }
+        Thread thread = new Thread(()->{
+            for (int i = 0; i < 5; i++) {
+                System.out.println("value: "+ i);
+            }
+            flag = false;
+            System.out.println("Flag status change to: "+  flag);
+        });
+
+        Thread thread1 = new Thread(()->{
+            int x =0;
+            while(flag){
+                x++;
+            }
+            System.out.println("value X : "+x);
+        });
+        thread.start();
+        thread1.start();
 
     }
 }
