53c2d04c4a1f669f8a8d6a83b9dc44727cb25037
kokon322 <chornuy322@gmail.com>
2020-12-30 21:25:24 +0100

    ContentPrinter ready

--- src/homeWork9/ContentPrinter.java
+++ src/homeWork9/ContentPrinter.java
@@ -1,4 +1,25 @@
 package homeWork9;
 
-public class ContentPrinter {
+public class ContentPrinter implements Runnable{
+    private LineStorage lineStorage;
+
+    public ContentPrinter(LineStorage lineStorage) {
+        this.lineStorage = lineStorage;
+    }
+
+    @Override
+    public void run() {
+        synchronized (lineStorage){
+            while (!lineStorage.isFinished().get()){
+                try {
+                    lineStorage.wait();
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+                System.out.println(lineStorage.getLine());
+                lineStorage.notify();
+            }
+            System.out.println("Done");
+        }
+    }
 }
